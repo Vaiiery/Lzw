c18c0ccbfa85ec259771db2534b06e7eb55bb323
Luis Angel Ramirez <20180570@ce.pucmm.edu.do>
2021-02-15 23:43:05 -0400

    Mejoras a ByteArray

--- ByteArray.java
+++ ByteArray.java
@@ -36,4 +36,53 @@ public class ByteArray {
         this.interno[index] = value;
     }
     
+    public ByteArray append(final ByteArray another) {
+        final int size = this.size();
+        final int anotherSize = another.size();
+        final int nuevoSize = size + anotherSize;
+        final byte[] nuevoBuff = new byte[nuevoSize];
+        for (int i = 0; i < size; ++i) {
+            nuevoBuff[i] = this.get(i);
+        }
+        for (int i = 0; i < anotherSize; ++i) {
+            nuevoBuff[i + size] = another.get(i);
+        }
+        this.interno = nuevoBuff;
+        return this;
+    }
+    
+    public ByteArray append(final byte[] array) {
+        return this.append(new ByteArray(array));
+    }
+    
+    public ByteArray append(final byte b1, final byte... bytes) {
+        return this.append(new ByteArray(b1, bytes));
+    }
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        final ByteArray other = (ByteArray)obj;
+        return Arrays.equals(this.interno, other.interno);
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = 31 * result + Arrays.hashCode(this.interno);
+        return result;
+    }
+    
+    @Override
+    public String toString() {
+        return "ByteArray [interno=" + Arrays.toString(this.interno) + "]";
+    }
+
 }
